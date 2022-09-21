facd88eecd4d7ef8e00cabf3c6d9f568a29b0967
LukasDarrieux <lukas.darrieux@gmail.com>
2022-09-20 22:27:44 -0300

    Removendo arquivos não utilizados, e refatorando alguns arquivos

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -48,6 +48,7 @@ export default class Calculator extends Component {
                 operation: equals ? null : operation,
                 current: equals ? 0 : 1,
                 clearDisplay: !equals, 
+
             })
         }
     }
